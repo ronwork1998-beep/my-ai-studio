92b753801c876c9f88a0006508ed8f392af05f0c
qaz82975 <76080228+qaz82975@users.noreply.github.com>
2026-05-12 13:37:06 +0800

    fix: update base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-ai-studio/', // 這裡填入您的 GitHub 儲存庫名稱，前後都要斜線
 })
